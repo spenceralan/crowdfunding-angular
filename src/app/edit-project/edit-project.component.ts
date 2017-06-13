6c00334e5a87a7da9eb74a4af127c17bc67483da
Kai de Rochemont and Spencer Alan <spencer@>
2017-06-13 14:17:15 -0700

    add edit page

--- src/app/edit-project/edit-project.component.ts
+++ src/app/edit-project/edit-project.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Project } from '../project.model';
+import { FirebaseService } from '../firebase.service';
 
 @Component({
   selector: 'app-edit-project',
   templateUrl: './edit-project.component.html',
-  styleUrls: ['./edit-project.component.css']
+  styleUrls: ['./edit-project.component.css'],
+  providers: [FirebaseService]
 })
 export class EditProjectComponent implements OnInit {
-
-  constructor() { }
+  projectID: string;
+  project;
+  constructor(private route: ActivatedRoute, private firebaseService: FirebaseService, private router: Router) { }
 
   ngOnInit() {
+    this.projectID = this.route.params['_value']['id'];
+    this.firebaseService.getProjectByID(this.projectID).subscribe(data => { this.project = data });
+  }
+
+  editProject(project){
+    this.firebaseService.editProject(project);
+    this.router.navigate([''])
   }
 
 }
--- src/app/firebase.service.ts
+++ src/app/firebase.service.ts
@@ -13,8 +13,23 @@ export class FirebaseService {
     return this.projects;
   }
 
+  getProjectByID(key){
+    return this.database.object('projects/' + key);
+  }
+
   saveProject(newProject){
     this.projects.push(newProject);
+  }
+
+  editProject(project){
+    let dbProject = this.getProjectByID(project.$key);
+    dbProject.update({
+      swag: project.swag,
+      title: project.title,
+      managers: project.managers,
+      description: project.description,
+      fundingTarget: project.fundingTarget
+    });
   }
 
 }
